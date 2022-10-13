b2934d08ec6df10307a9d86e409a0ef2a25bb424
hannat <shoroyehannat@gmail.com>
2022-10-13 09:22:48 -0700

    Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,9 @@
 #include <stdio.h>
 
 /**
- * main - print the phrase "Programming is like building a multilingual puzzle"
- * Return: 0 if exited properly
+ * main - Printing
+ *
+ * Return: Always 0
  */
 int main(void)
 {
--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,13 +1,14 @@
 #include <stdio.h>
 
 /**
- * main - print the phrase with proper grammer,
- * but the outcome is a piece of art
+ * main - Prints a text
  *
- * Return: 0 if exited properly,
+ * Return: Always 0
  */
 int main(void)
 {
-   	printf("with proper grammar, but the outcome is a piece of art,\n");
-	return(0);
-}	
+	printf("with proper grammer, but the outcome is a piece of art,\n");
+	return (0);
+}
+
+
